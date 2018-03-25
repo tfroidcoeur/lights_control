0cc6d66fc7d3039aed4e746d6fbbddfa4b2a8ba0
Tim Froidcoeur <tim.froidcoeur@gmail.com>
2018-03-25 08:43:10 +0200

    created main controller class to contain all the previous static arrays and stuff

---
+++ src/Controller.h
@@ -0,0 +1,43 @@
+/*
+ * light.h
+ *
+ *  Created on: Mar 19, 2018
+ *      Author: fraco
+ */
+
+#ifndef CONTROLLER_H_
+#define CONTROLLER_H_
+
+#include "Actor.h"
+#include "InPin.h"
+#include "OutPin.h"
+#include "MotionSpot.h"
+#include "Teleruptor.h"
+#include "Runner.h"
+#include "SimpleButton.h"
+#include <vector>
+
+class Controller: public Actor {
+public:
+	Controller();
+	virtual ~Controller();
+	void handle();
+	void setup();
+private:
+	void setupMotionSpots();
+	void connectMotionSpot(MotionSpot & spot, Button & but, sigslot::signal0<> & butshort, sigslot::signal0<> & butlong);
+	vector<InPin> inpinA;
+	vector<OutPin> relay;
+	vector<OutPin> outpinD;
+	vector<InPin>inpinInt;
+	vector<Teleruptor> teleruptors;
+	vector<MotionSpot> spot;
+	vector<SimpleButton> spotButtons;
+	Runner r;
+};
+
+
+
+
+
+#endif /* CONTROLLER_H_ */
