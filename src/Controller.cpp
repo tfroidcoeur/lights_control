f180dfaa9374c4de8844a2756a3111646a83fefa
Tim Froidcoeur <tim.froidcoeur@gmail.com>
2018-03-25 08:43:10 +0200

    created main controller class to contain all the previous static arrays and stuff

---
+++ src/Controller.cpp
@@ -0,0 +1,130 @@
+/*
+ * Controller.cpp
+ *
+ *  Created on: Mar 25, 2018
+ *      Author: fraco
+ */
+#include <Controllino.h>  /* Usage of CONTROLLINO library allows you to use CONTROLLINO_xx aliases in your sketch. */
+#include <HardwareSerial.h>
+#include "Controller.h"
+#include "Runner.h"
+#include "SimpleButton.h"
+
+
+Controller::Controller(){
+	// create pins
+
+	// inpinsA
+	inpinA.push_back(InPin(CONTROLLINO_A0));
+	inpinA.push_back(InPin(CONTROLLINO_A1));
+	inpinA.push_back(InPin(CONTROLLINO_A2));
+	inpinA.push_back(InPin(CONTROLLINO_A3));
+	inpinA.push_back(InPin(CONTROLLINO_A4));
+	inpinA.push_back(InPin(CONTROLLINO_A5));
+	inpinA.push_back(InPin(CONTROLLINO_A6));
+	inpinA.push_back(InPin(CONTROLLINO_A7));
+	inpinA.push_back(InPin(CONTROLLINO_A8));
+	inpinA.push_back(InPin(CONTROLLINO_A9));
+
+	// inpin INT
+	inpinInt.push_back(InPin(CONTROLLINO_IN0));
+	inpinInt.push_back(InPin(CONTROLLINO_IN1));
+	// outpins, relay
+	relay.push_back(OutPin(CONTROLLINO_RELAY_00));
+	relay.push_back(OutPin(CONTROLLINO_RELAY_01));
+	relay.push_back(OutPin(CONTROLLINO_RELAY_02));
+	relay.push_back(OutPin(CONTROLLINO_RELAY_03));
+	relay.push_back(OutPin(CONTROLLINO_RELAY_04));
+	relay.push_back(OutPin(CONTROLLINO_RELAY_05));
+	relay.push_back(OutPin(CONTROLLINO_RELAY_06));
+	relay.push_back(OutPin(CONTROLLINO_RELAY_07));
+	relay.push_back(OutPin(CONTROLLINO_RELAY_08));
+	relay.push_back(OutPin(CONTROLLINO_RELAY_09));
+
+	// outpins, digital
+	outpinD.push_back(OutPin(CONTROLLINO_D0));
+	outpinD.push_back(OutPin(CONTROLLINO_D1));
+	outpinD.push_back(OutPin(CONTROLLINO_D2));
+	outpinD.push_back(OutPin(CONTROLLINO_D3));
+	outpinD.push_back(OutPin(CONTROLLINO_D4));
+	outpinD.push_back(OutPin(CONTROLLINO_D5));
+	outpinD.push_back(OutPin(CONTROLLINO_D6));
+	outpinD.push_back(OutPin(CONTROLLINO_D7));
+	outpinD.push_back(OutPin(CONTROLLINO_D8));
+	outpinD.push_back(OutPin(CONTROLLINO_D9));
+
+	// teleruptors
+	teleruptors.push_back(Teleruptor(inpinA[0], relay[0]));
+	teleruptors.push_back(Teleruptor(inpinA[1], relay[1]));
+	teleruptors.push_back(Teleruptor(inpinA[2], relay[2]));
+	teleruptors.push_back(Teleruptor(inpinA[3], relay[3]));
+	teleruptors.push_back(Teleruptor(inpinA[4], relay[4]));
+	teleruptors.push_back(Teleruptor(inpinA[5], relay[5]));
+	teleruptors.push_back(Teleruptor(inpinA[6], relay[6]));
+	teleruptors.push_back(Teleruptor(inpinA[7], relay[7]));
+	teleruptors.push_back(Teleruptor(inpinA[8], relay[8]));
+	teleruptors.push_back(Teleruptor(inpinA[9], relay[9]));
+
+	// create spots
+	spot.push_back(
+			MotionSpot(Controller::outpinD[0], Controller::outpinD[1],
+					Controller::outpinD[2]));
+	spot.push_back(
+			MotionSpot(Controller::outpinD[3], Controller::outpinD[4],
+					Controller::outpinD[5]));
+
+	// create spot buttons
+	spotButtons.push_back(SimpleButton(Controller::inpinInt[0]));
+	spotButtons.push_back(SimpleButton(Controller::inpinInt[0]));
+
+}
+
+Controller::~Controller(){
+	spot.clear();
+
+}
+
+void Controller::connectMotionSpot(MotionSpot & spot, Button & but, sigslot::signal0<> & butshort, sigslot::signal0<> & butlong) {
+	butshort.connect(&spot,&MotionSpot::shortpressed);
+	butlong.connect(&spot,&MotionSpot::longpressed);
+}
+
+void Controller::setupMotionSpots() {
+	connectMotionSpot(spot[0],spotButtons[0], spotButtons[0].shortpress, spotButtons[0].longpress);
+	connectMotionSpot(spot[1],spotButtons[1], spotButtons[1].shortpress, spotButtons[0].longpress);
+
+	for (unsigned long i = 0; i < sizeof(spot) / sizeof(class MotionSpot); i++) {
+		r.addActor(&spot[i]);
+	}
+}
+
+void Controller::setup() {
+	vector<InPin>::iterator it;
+	for (it = inpinA.begin(); it != inpinA.end(); it++) {
+		r.addActor(&(*it));
+	}
+	for (it = inpinInt.begin(); it != inpinInt.end(); it++) {
+		r.addActor(&(*it));
+	}
+
+	vector<Teleruptor>::iterator teit;
+	for (teit = teleruptors.begin(); teit!=teleruptors.end() ; teit++) {
+		r.addActor(&(*teit));
+	}
+
+	vector<SimpleButton>::iterator butit;
+	for (butit = spotButtons.begin(); butit!=spotButtons.end() ; butit++) {
+		r.addActor(&(*butit));
+	}
+
+
+
+	// TODO add pins and buttons
+	setupMotionSpots();
+	r.setup();
+}
+
+void Controller::handle() {
+	r.handle();
+}
+
--- src/lights.cpp
+++ src/lights.cpp
@@ -1,123 +1,19 @@
 #include <Controllino.h>  /* Usage of CONTROLLINO library allows you to use CONTROLLINO_xx aliases in your sketch. */
 #include <HardwareSerial.h>
+#include "Controller.h"
 
-#include "MotionSpot.h"
-#include "Runner.h"
-#include "Teleruptor.h"
 
-// create pins
-
-// inpinsA
-InPin inpinA[] = {
-		InPin(CONTROLLINO_A0),
-		InPin(CONTROLLINO_A1),
-		InPin(CONTROLLINO_A2),
-		InPin(CONTROLLINO_A3),
-		InPin(CONTROLLINO_A4),
-		InPin(CONTROLLINO_A5),
-		InPin(CONTROLLINO_A6),
-		InPin(CONTROLLINO_A7),
-		InPin(CONTROLLINO_A8),
-		InPin(CONTROLLINO_A9),
-};
-
-// inpin INT
-InPin inpinInt[] = {
-		InPin(CONTROLLINO_IN0),
-		InPin(CONTROLLINO_IN1),
-};
-
-// outpins, relay
-OutPin relay[] = {
-		OutPin(CONTROLLINO_RELAY_00),
-		OutPin(CONTROLLINO_RELAY_01),
-		OutPin(CONTROLLINO_RELAY_02),
-		OutPin(CONTROLLINO_RELAY_03),
-		OutPin(CONTROLLINO_RELAY_04),
-		OutPin(CONTROLLINO_RELAY_05),
-		OutPin(CONTROLLINO_RELAY_06),
-		OutPin(CONTROLLINO_RELAY_07),
-		OutPin(CONTROLLINO_RELAY_08),
-		OutPin(CONTROLLINO_RELAY_09),
-};
-
-// outpins, digital
-OutPin outpinD[] = {
-		OutPin(CONTROLLINO_D0),
-		OutPin(CONTROLLINO_D1),
-		OutPin(CONTROLLINO_D2),
-		OutPin(CONTROLLINO_D3),
-		OutPin(CONTROLLINO_D4),
-		OutPin(CONTROLLINO_D5),
-		OutPin(CONTROLLINO_D6),
-		OutPin(CONTROLLINO_D7),
-		OutPin(CONTROLLINO_D8),
-		OutPin(CONTROLLINO_D9),
-};
+static Controller controller;
 
 // the setup function runs once when you press reset (CONTROLLINO RST button) or connect the CONTROLLINO to the PC
-Teleruptor teleruptors[] = {
-		Teleruptor( inpinA[0], relay[0]) ,
-		Teleruptor( inpinA[1], relay[1]),
-		Teleruptor( inpinA[2], relay[2]),
-		Teleruptor( inpinA[3], relay[3]),
-		Teleruptor( inpinA[4], relay[4]),
-		Teleruptor( inpinA[5], relay[5]),
-		Teleruptor( inpinA[6], relay[6]),
-		Teleruptor( inpinA[7], relay[7]),
-		Teleruptor( inpinA[8], relay[8]),
-		/*Teleruptor( inpinA[9], relay[9]),*/
-};
-
-// TODO define pins
-// TODO define buttons
-
-Button spotButton0(inpinInt[0]);
-Button spotButton1(inpinInt[1]);
-sigslot::signal0<> button0Short;
-sigslot::signal0<> button0Long;
-sigslot::signal0<> button1Short;
-sigslot::signal0<> button1Long;
-// TODO setup modes and connect to motionspot
-MotionSpot spot[] =
-		{
-				MotionSpot(outpinD[0],outpinD[1],outpinD[2] ),
-				MotionSpot(outpinD[3],outpinD[4],outpinD[5] ),
-		};
-Runner r;
-
-void connectMotionSpot(MotionSpot & spot, Button & but, sigslot::signal0<> & butshort, sigslot::signal0<> & butlong) {
-	but.addMode(ButtonMode(20, "short", &butshort));
-	but.addMode(ButtonMode(2000, "long", &butlong));
-	butshort.connect(&spot,&MotionSpot::shortpressed);
-	butlong.connect(&spot,&MotionSpot::longpressed);
-}
-
-void setupMotionSpots() {
-	connectMotionSpot(spot[0],spotButton0, button0Short, button0Long);
-	connectMotionSpot(spot[1],spotButton1, button1Short, button1Long);
-
-	for (unsigned long i = 0; i < sizeof(spot) / sizeof(class MotionSpot); i++) {
-		r.addActor(&spot[i]);
-	}
-}
-
 void setup() {
 	Serial.begin(9600);
-
-	for (unsigned int i = 0; i < sizeof(teleruptors) / sizeof(struct Teleruptor); i++) {
-		r.addActor(&teleruptors[i]);
-	}
-
-	// TODO add pins and buttons
-	setupMotionSpots();
-
-	r.setup();
+	controller.setup();
 
 	Serial.println("setup done");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-	r.handle();
+	controller.handle();
 }
