f976f42c67332bdfaf617215e5f931f5c9d5f832
Tim Froidcoeur <tim.froidcoeur@gmail.com>
2018-04-07 09:06:26 +0200

    add Switchable interface to teleruptor

--- src/Teleruptor.h
+++ src/Teleruptor.h
@@ -9,15 +9,22 @@
 
 #include "InPin.h"
 #include "OutPin.h"
+#include "sigslot.h"
+#include "Switchable.h"
 
 
-class Teleruptor: public Actor, public sigslot::has_slots<> {
+class Teleruptor: public Actor, public sigslot::has_slots<>, public Switchable {
 public:
 	Teleruptor(InPin & inpin , OutPin & outpin);
 	virtual ~Teleruptor();
 	virtual void handle(void);
 	virtual void setup(void);
 	void notifyInPin(int value);
+// Switchable
+	virtual void on();
+	virtual void off();
+	virtual bool isOn();
+	virtual void toggle();
 private:
 	InPin &in;
 	OutPin &out;
