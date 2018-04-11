092a12f79797252d61ea87f812da705638ebe4d1
Tim Froidcoeur <tim.froidcoeur@gmail.com>
2018-04-11 06:33:30 +0200

    implement FunAction and ActionList
    Signed-off-by: Tim Froidcoeur <tim.froidcoeur@tessares.net>

--- src/Action.h
+++ src/Action.h
@@ -8,9 +8,46 @@
 #ifndef ACTION_H_
 #define ACTION_H_
 
+#include <list>
+
+#include "sigslot.h"
+
 class Action {
 public:
 	virtual void doit() = 0;
+};
+
+template<class Target>
+class FunAction: public Action {
+public:
+	FunAction(Target * target, void (Target::*fun)()) :
+			fun(fun), target(target) {
+	}
+	;
+	void doit() {
+		(target->*fun)();
+	}
+private:
+	void (Target::*fun)();
+	Target * target;
+};
+
+class ActionList: public Action, public sigslot::has_slots<> {
+
+public:
+	ActionList(){};
+	void append(Action * act) {
+		actions.push_back(act);
+	}
+
+	void doit() {
+		std::list<Action *>::iterator it = actions.begin();
+		for (; it != actions.end(); it++) {
+			(*it)->doit();
+		}
+	}
+private:
+	std::list<Action *> actions;
 };
 
 #endif /* ACTION_H_ */
