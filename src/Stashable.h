464201d4bba59134aa8768b8b1d5dafa9f836604
Tim Froidcoeur <tim.froidcoeur@gmail.com>
2018-04-14 08:03:46 +0200

    add stashable to teleruptor
    Signed-off-by: Tim Froidcoeur <tim.froidcoeur@tessares.net>

---
+++ src/Stashable.h
@@ -0,0 +1,19 @@
+/*
+ * Stashable.h
+ *
+ *  Created on: Apr 11, 2018
+ *      Author: fraco
+ */
+
+#ifndef STASHABLE_H_
+#define STASHABLE_H_
+
+class Stashable {
+public:
+	Stashable(){};
+	virtual ~Stashable(){};
+	virtual void save()=0;
+	virtual void restore()=0;
+};
+
+#endif /* STASHABLE_H_ */
--- src/Teleruptor.h
+++ src/Teleruptor.h
@@ -10,13 +10,17 @@
 #include "InPin.h"
 #include "OutPin.h"
 #include "sigslot.h"
+#include "Stashable.h"
 #include "Switchable.h"
 
 
-class Teleruptor: public Actor, public sigslot::has_slots<>, public Switchable {
+class Teleruptor: public Actor,
+		public sigslot::has_slots<>,
+		public Switchable,
+		public Stashable {
 public:
-	Teleruptor(InPin & inpin , OutPin & outpin);
-    Teleruptor( sigslot::signal0<> & sig, OutPin & outpin);
+	Teleruptor(InPin & inpin, OutPin & outpin);
+	Teleruptor(sigslot::signal0<> & sig, OutPin & outpin);
 	virtual ~Teleruptor();
 	virtual void handle(void);
 	virtual void setup(void);
@@ -26,8 +30,12 @@ public:
 	virtual void off();
 	virtual bool isOn();
 	virtual void toggle();
+// Stashable
+	virtual void save();
+	virtual void restore();
 private:
 	OutPin &out;
+	bool savedstate;
 };
 
 #endif /* TELERUPTOR_H_ */
